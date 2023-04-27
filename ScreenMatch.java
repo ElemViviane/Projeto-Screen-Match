6bb2f7196cc1af210ff0b6b39509524582c7d6a0
Elem Viviane <131198461+ElemViviane@users.noreply.github.com>
2023-04-27 20:32:13 -0300

    Criando

---
+++ ScreenMatch.java
@@ -0,0 +1,55 @@
+public class Filme {
+//métodos da class
+	String nome;
+	int anoLancamento;
+	boolean incluidoNoPlano;
+	private double somaDasAvaliacoes;
+	private int totalDeAvaliacoes;
+	int duracaoEmMinutos;
+	
+	
+	//método de ficha técnica, que já passa o nome do filme, ano de lançamento e duração.
+	void exibeFichaTecnica() {
+		System.out.println("Nome: " + nome);
+		System.out.println("Ano de Lançamento: " + anoLancamento);
+		System.out.println("Duração: " + duracaoEmMinutos + " Minutos");
+	}
+	//método de somar e incrementar as avaliações.
+	public void avalia(double nota) {
+		somaDasAvaliacoes += nota;
+		totalDeAvaliacoes ++;
+	}
+	
+	//método para retornar média das notas.
+	public double getMedia() {
+		return somaDasAvaliacoes / totalDeAvaliacoes;
+	}
+
+	//método para retornar o total de avaliações.
+	public int getTotalDeAvaliacoes() {
+		return totalDeAvaliacoes;
+	}
+}
+
+
+//testando a funcionalidade.
+public class Principal {
+	public static void main(String[] args) {
+		
+		Filme meuFilme = new Filme();
+		meuFilme.nome = "A Culpa É das Estrelas";
+		meuFilme.anoLancamento = 2014;
+		meuFilme.duracaoEmMinutos = 126;
+		
+		meuFilme.exibeFichaTecnica();
+		
+		meuFilme.avalia(8);
+		meuFilme.avalia(10);
+		meuFilme.avalia(7);
+		meuFilme.avalia(7.5);
+		
+		System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+		System.out.println("Média das avaliações: " + meuFilme.getMedia());
+		
+	}
+}
