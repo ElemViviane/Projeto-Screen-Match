70d5a0c10a1b20bba57692d90d3acf59f9cc18c2
Elem Viviane <131198461+ElemViviane@users.noreply.github.com>
2023-04-27 21:39:10 -0300

    Editando

--- ScreenMatch.java
+++ ScreenMatch.java
@@ -2,12 +2,12 @@ package br.com.alura.screenmatch.modelos;
 //Projeto Screen Match.
 public class Filme {
 //métodos da class.
-	public String nome;
-	public int anoLancamento;
-	public boolean incluidoNoPlano;
+	private String nome;
+	private int anoLancamento;
+	private boolean incluidoNoPlano;
 	private double somaDasAvaliacoes;
 	private int totalDeAvaliacoes;
-	public int duracaoEmMinutos;
+	private int duracaoEmMinutos;
 	
 	
 	//método de ficha técnica, que já passa o nome do filme, ano de lançamento e duração.
@@ -15,6 +15,7 @@ public class Filme {
 		System.out.println("Nome: " + nome);
 		System.out.println("Ano de Lançamento: " + anoLancamento);
 		System.out.println("Duração: " + duracaoEmMinutos + " Minutos");
+		System.out.println("Incluido no plano: " + incluidoNoPlano);
 	}
 	//método de somar e incrementar as avaliações.
 	public void avalia(double nota) {
@@ -31,6 +32,55 @@ public class Filme {
 	public int getTotalDeAvaliacoes() {
 		return totalDeAvaliacoes;
 	}
+	
+	public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
+		this.totalDeAvaliacoes = totalDeAvaliacoes;
+	}
+	
+	//método para retornar o nome.
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//método para retornar o ano de lançamento.
+	public int getAnoLancamento() {
+		return anoLancamento;
+	}
+	
+	public void setAnoLancamento(int anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+	
+	//método para retornar se esta incluso no plano.
+	public boolean getIncluidoNoPlano() {
+		return incluidoNoPlano;
+	}
+	
+	public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+		this.incluidoNoPlano = incluidoNoPlano;
+	}
+	
+	//método para retornar a soma das avaliações.
+	public double getSomaDasAvaliacoes() {
+		return somaDasAvaliacoes;
+	}
+	
+	public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
+		this.somaDasAvaliacoes = somaDasAvaliacoes;
+	}
+	
+	//método para retornar a duração do filme em minutos.
+	public int getDuracaoEmMinutos() {
+		return duracaoEmMinutos;
+	}
+	
+	public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+		this.duracaoEmMinutos = duracaoEmMinutos;
+	}
 }
 
 
@@ -40,9 +90,10 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Filme meuFilme = new Filme();
-		meuFilme.nome = "A Culpa É das Estrelas";
-		meuFilme.anoLancamento = 2014;
-		meuFilme.duracaoEmMinutos = 126;
+		meuFilme.setNome("A Culpa É das Estrelas");
+		meuFilme.setAnoLancamento(2014);
+		meuFilme.setDuracaoEmMinutos(126);
+	        meuFilme.setIncluidoNoPlano(true);
 		
 		meuFilme.exibeFichaTecnica();
 		
