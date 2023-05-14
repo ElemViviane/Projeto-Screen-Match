dda2a055a27fcaa595e2e55db924e01609542b8e
Elem Viviane <131198461+ElemViviane@users.noreply.github.com>
2023-05-14 11:56:50 -0300

    Editando

--- ClassCalculadoraDeTempo.java
+++ ClassCalculadoraDeTempo.java
@@ -1,6 +1,6 @@
-package br.com.alura.screenmatch.calculos;
+package br.com.screenmatch.calculos;
 
-import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.screenmatch.modelos.Titulo;
 
 public class CalculadoraDeTempo {
 
