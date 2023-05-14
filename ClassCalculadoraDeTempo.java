1931bd4b1ec03d849ae1de08ae104180c584d686
Elem Viviane <131198461+ElemViviane@users.noreply.github.com>
2023-05-14 11:52:29 -0300

    Criando

---
+++ ClassCalculadoraDeTempo.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+
+	private int tempoTotal;
+	
+	public int getTempoTotal() {
+		return tempoTotal;
+	}
+	
+	public void inclui(Titulo titulo) {
+		this.tempoTotal += titulo.getDuracaoEmMinutos();
+	}
+}
